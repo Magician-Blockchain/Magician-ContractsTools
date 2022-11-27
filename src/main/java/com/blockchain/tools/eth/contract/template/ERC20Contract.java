6aa45460caa25e073d8d0d141c507cf6642edb55
Beerus <yuyemail@163.com>
2022-11-27 19:41:46 +0800

    init commit

--- src/main/java/com/blockchain/tools/eth/contract/template/ERC20Contract.java
+++ src/main/java/com/blockchain/tools/eth/contract/template/ERC20Contract.java
@@ -82,8 +82,8 @@ public class ERC20Contract {
      * @return
      * @throws IOException
      */
-    public Boolean allowance(String owner, String spender) throws IOException {
-        return Commons.resultBool(ethContractUtil, contractAddress,
+    public BigInteger allowance(String owner, String spender) throws IOException {
+        return Commons.resultBigInteger(ethContractUtil, contractAddress,
                 EthAbiCodecTool.getInputData(
                         "allowance",
                         new Address(owner),
